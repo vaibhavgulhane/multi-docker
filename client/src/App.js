59cbc29c55d7ba274ca4ac9e8d69b7a8bd761c56
unknown <vaibhavgulhane@yahoo.co.in>
2020-03-01 19:59:42 +0530

    version 2 for k8ts

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,21 @@ import otherPage from './OtherPage';
 import Fib from './Fib';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Fib Calculator</h1>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">OtherPage</Link>
-        </header>
-        <div>
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={otherPage} />
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Fib Calculator version 2</h1>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">OtherPage</Link>
+          </header>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={otherPage} />
+          </div>
         </div>
-      </div>
-    </Router>
-  );
+      </Router>
+    );
 }
 
 export default App;
