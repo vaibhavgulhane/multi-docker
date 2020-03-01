12f31a8b4a527fa77706e7d20786be0675b64c93
unknown <vaibhavgulhane@yahoo.co.in>
2020-03-01 20:07:11 +0530

    change for k8ts

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <div className="App">
           <header className="App-header">
-            <h1 className="App-title">Fib Calculator version 2</h1>
+            <h1 className="App-title">Fib Calc version 2</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">OtherPage</Link>
           </header>
